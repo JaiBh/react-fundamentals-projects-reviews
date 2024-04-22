b8288bdbf77ffeed669c3981383f9ff92d358e32
Jai Bhullar <81278507+JaiBh@users.noreply.github.com>
2024-04-22 15:10:13 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,37 @@
+import { useState } from "react";
+import reviews from "./data";
+import { FaBeer } from "react-icons/fa";
+import User from "./components/user";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const selectedUser = reviews[index];
+  const randomUser = () => {
+    const random = Math.floor(Math.random() * reviews.length);
+    setIndex(random);
+  };
+  const nextUser = (next) => {
+    if (next) {
+      setIndex((currentIndex) => {
+        return (currentIndex + 1) % reviews.length;
+      });
+    } else {
+      setIndex((currentIndex) => {
+        return (currentIndex - 1 + reviews.length) % reviews.length;
+      });
+    }
+  };
+
+  console.log(index);
+  return (
+    <div>
+      <User
+        key={selectedUser.id}
+        user={selectedUser}
+        randomUser={randomUser}
+        nextUser={nextUser}
+      ></User>
+    </div>
+  );
 };
 export default App;
